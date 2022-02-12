4579b402e10cb8fe16015ae3ef1c68966f2d94b9
Amardeep <kumaramardipsingh@gmail.com>
2022-02-12 10:50:19 +0530

    add changed to publish on pypi

--- setup.py
+++ setup.py
@@ -1,21 +1,23 @@
 from setuptools import find_packages, setup
+import os
 
-with open("requirements.txt") as f:
-    required = f.read().splitlines()
 
 setup(
     name="dlkp",
-    version="0.1",
-    description="A deep learning library for keyphrase extraction and generation",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
+    version="0.3",
     author="Amardeep Kumar || Debanjan Mahata",
-    author_email="Kumaramardipsingh@gmail.com || debanjanmahata85@gmail.com",
+    author_email="kumaramardipsingh@gmail.com",
+    description="A deep learning library for keyphrase extraction and generation",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/midas-research/dlkp",
     download_url="https://github.com/midas-research/dlkp/archive/refs/tags/0.1.tar.gz",
-    packages=find_packages(exclude="tests"),
+    project_urls={
+        "Bug Tracker": "https://github.com/midas-research/dlkp/issues",
+    },
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     license="Apache License Version 2.0",
-    install_requires=required,
     include_package_data=True,
-    python_requires=">=3.8.10",
+    python_requires=">=3.7",
 )
---
+++ temp.py
@@ -0,0 +1,13 @@
+requires = [
+    "setuptools>=42",
+    "wheel",
+    "numpy",
+    "requests",
+    "tqdm>=4.47.0",
+    "regex",
+    "transformers>=4.6.0",
+    "datasets" "seqeval",
+    "torch",
+    "pandas",
+    "tokenizers",
+]
