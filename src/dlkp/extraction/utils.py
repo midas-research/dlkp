28d47a974cd615ef1db64dad183773517155d493
Amardeep <kumaramardipsingh@gmail.com>
2022-03-31 20:51:01 +0530

    fix max seq len bug

--- src/dlkp/extraction/utils.py
+++ src/dlkp/extraction/utils.py
@@ -121,8 +121,15 @@ class KEDataArguments:
         default=None,
         metadata={"help": "The number of processes to use for the preprocessing."},
     )
+    max_seq_length: int = field(
+        default=None,
+        metadata={
+            "help": "The maximum total input sequence length after tokenization. Sequences longer "
+            "than this will be truncated, sequences shorter will be padded."
+        },
+    )
     pad_to_max_length: bool = field(
-        default=False,
+        default=True,
         metadata={
             "help": "Whether to pad all samples to model maximum sentence length. "
             "If False, will pad the samples dynamically when batching to the maximum length in the batch. More "
